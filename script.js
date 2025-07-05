3e9c322d1002ea24f90745441d531d014b71f98c
rgwdbandara <rgwdbandara@nsbm.students.ac.lk>
2025-07-05 12:58:43 +0530

    About Section

--- script.js
+++ script.js
@@ -3,11 +3,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const closeBtn = document.getElementById("menu-close-button");
   const navMenu = document.getElementById("nav-menu");
 
-  openBtn.addEventListener("click", () => {
-    navMenu.classList.add("open");
-  });
+  if (openBtn && navMenu) {
+    openBtn.addEventListener("click", () => {
+      navMenu.classList.add("open");
+    });
+  }
 
-  closeBtn.addEventListener("click", () => {
-    navMenu.classList.remove("open");
-  });
+  if (closeBtn && navMenu) {
+    closeBtn.addEventListener("click", () => {
+      navMenu.classList.remove("open");
+    });
+  }
 });
